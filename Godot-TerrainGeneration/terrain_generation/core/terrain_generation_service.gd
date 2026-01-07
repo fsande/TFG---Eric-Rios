079ec1a8c07cd87bc923a54af69589ba86d8ac97
Eric Ríos <alu0101549835@ull.edu.es>
2026-01-07 18:21:38 +0000

    TerrainGenerationService method restructure

--- Godot-TerrainGeneration/terrain_generation/core/terrain_generation_service.gd
+++ Godot-TerrainGeneration/terrain_generation/core/terrain_generation_service.gd
@@ -14,63 +14,12 @@ var last_mesh_modifier_context: MeshModifierContext = null
 func _init():
 	mesh_builder = TerrainMeshBuilder.new()
 
-func _generate_heightmap(config: TerrainConfiguration, processing_context: ProcessingContext) -> Image:
-	processing_context.mesh_params = config.mesh_generator_parameters
-	return config.heightmap_source.generate(processing_context)
-
-func _execute_mesh_modification_pipeline(config: TerrainConfiguration, processing_context: ProcessingContext, heightmap: Image, mesh_result: MeshGenerationResult) -> void:
-	var scene_root: Node3D = null
-	if config.mesh_modification_pipeline:
-#		var pipeline_start := Time.get_ticks_msec()
-		var initial_terrain_data := TerrainData.new(heightmap, mesh_result, Vector2(config.terrain_size, config.terrain_size), null, {}, 0)
-		var modifier_context := config.mesh_modification_pipeline.execute(
-			initial_terrain_data,
-			processing_context,
-			scene_root,
-			processing_context.mesh_params
-		)
-		if modifier_context:
-			last_mesh_modifier_context = modifier_context
-			mesh_result = modifier_context.get_mesh_data()
-			scene_root = modifier_context.scene_root			
-#			var pipeline_time := Time.get_ticks_msec() - pipeline_start
-		else:
-			push_warning("TerrainGenerationService: Mesh modification pipeline failed")
-
-func _create_terrain_data(config: TerrainConfiguration, heightmap: Image, mesh_result: MeshGenerationResult, collision: Shape3D, total_time: int) -> TerrainData:
-	var metadata := {
-		"heightmap_metadata": config.heightmap_source.get_metadata(),
-		"configuration": {
-			"size": config.mesh_generator_parameters.mesh_size,
-			"subdivisions": config.mesh_generator_parameters.subdivisions,
-			"max_height": config.mesh_generator_parameters.height_scale,
-			"generation_seed": config.generation_seed
-		},
-		"mesh_modification_enabled": config.mesh_modification_pipeline != null,
-	}
-	return TerrainData.new(heightmap, mesh_result, Vector2(config.terrain_size, config.terrain_size), collision, metadata, total_time)
-
-func _generate_collision_shape(config: TerrainConfiguration, mesh_result: MeshGenerationResult) -> Shape3D:
-	if config.generate_collision:
-		return mesh_builder.build_collision(mesh_result)
-	return null
-
-func _set_cache(config: TerrainConfiguration, terrain_data: TerrainData) -> void:
-	var cache_key := _get_cache_key(config)
-	if config.enable_caching:
-		_cache[cache_key] = terrain_data
-
-func _generate_processing_context(config: TerrainConfiguration) -> ProcessingContext:
-	var processor_type := config.get_effective_processor_type()
-	return ProcessingContext.new(
-		config.terrain_size,
-		config.generation_seed,
-		processor_type
-	)
-
 ## Generate complete terrain data from a `TerrainConfiguration`.
 ## Returns a `TerrainData` object or null on failure.
 func generate(config: TerrainConfiguration) -> TerrainData:
+	return _generate(config)
+
+func _generate(config: TerrainConfiguration) -> TerrainData:
 	if not config or not config.is_valid():
 		push_error("TerrainGenerationService: Invalid configuration")
 		return null
@@ -127,3 +76,57 @@ func _get_cache_key(config: TerrainConfiguration) -> String:
 	if config.mesh_modification_pipeline:
 		key_parts.append(str(config.mesh_modification_pipeline.get_instance_id()))
 	return str(hash("".join(key_parts)))
+
+func _generate_processing_context(config: TerrainConfiguration) -> ProcessingContext:
+	var processor_type := config.get_effective_processor_type()
+	return ProcessingContext.new(
+		config.terrain_size,
+		config.generation_seed,
+		processor_type
+	)
+
+func _generate_heightmap(config: TerrainConfiguration, processing_context: ProcessingContext) -> Image:
+	processing_context.mesh_params = config.mesh_generator_parameters
+	return config.heightmap_source.generate(processing_context)
+
+func _execute_mesh_modification_pipeline(config: TerrainConfiguration, processing_context: ProcessingContext, heightmap: Image, mesh_result: MeshGenerationResult) -> void:
+	var scene_root: Node3D = null
+	if config.mesh_modification_pipeline:
+		var initial_terrain_data := TerrainData.new(heightmap, mesh_result, Vector2(config.terrain_size, config.terrain_size), null, {}, 0)
+		var modifier_context := config.mesh_modification_pipeline.execute(
+			initial_terrain_data,
+			processing_context,
+			scene_root,
+			processing_context.mesh_params
+		)
+		if modifier_context:
+			last_mesh_modifier_context = modifier_context
+			mesh_result = modifier_context.get_mesh_data()
+			scene_root = modifier_context.scene_root			
+		else:
+			push_warning("TerrainGenerationService: Mesh modification pipeline failed")
+
+func _generate_collision_shape(config: TerrainConfiguration, mesh_result: MeshGenerationResult) -> Shape3D:
+	if config.generate_collision:
+		return mesh_builder.build_collision(mesh_result)
+	return null
+
+func _create_terrain_data(config: TerrainConfiguration, heightmap: Image, mesh_result: MeshGenerationResult, collision: Shape3D, total_time: int) -> TerrainData:
+	var metadata := {
+		"heightmap_metadata": config.heightmap_source.get_metadata(),
+		"configuration": {
+			"size": config.mesh_generator_parameters.mesh_size,
+			"subdivisions": config.mesh_generator_parameters.subdivisions,
+			"max_height": config.mesh_generator_parameters.height_scale,
+			"generation_seed": config.generation_seed
+		},
+		"mesh_modification_enabled": config.mesh_modification_pipeline != null,
+	}
+	return TerrainData.new(heightmap, mesh_result, Vector2(config.terrain_size, config.terrain_size), collision, metadata, total_time)
+
+func _set_cache(config: TerrainConfiguration, terrain_data: TerrainData) -> void:
+	var cache_key := _get_cache_key(config)
+	if config.enable_caching:
+		_cache[cache_key] = terrain_data
+
+
